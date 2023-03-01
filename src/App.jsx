25ccdcfce0ee2e0a246f28ebea5e8de580c93ec6
dinako81 <dina.koliata@gmail.com>
2023-03-01 23:19:56 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import { v4 as uuidv4 } from "uuid";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">TEST</header>
+    </div>
+  );
+}
+
+export default App;
